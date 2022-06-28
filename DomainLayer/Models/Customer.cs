8e3ca4fa726b52ebef67cff4d5968b16af1dd0c9
TanvirHridoy <anxioushridoy4@gmail.com>
2022-06-28 15:48:41 +0600

    Add project files.

---
+++ DomainLayer/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainLayer.Models
+{
+   public class Customer : BaseEntity
+   {
+        public string CustomerName { get; set; }
+        public string PurchasesProduct { get; set; }
+        public string PaymentType { get; set; }
+   }
+}
---
+++ RepositoryLayer/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using DomainLayer.EntityMapper;
+using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+
+namespace RepositoryLayer
+{
+    public partial class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CustomerMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
